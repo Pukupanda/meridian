e0d3e4427d0299b683760d562b9170e2537281df
roshankc00 <120471898+roshankc00@users.noreply.github.com>
2024-07-13 00:42:10 +0545

    fixed ui

--- app/(flights)/flight/[flightId]/page.tsx
+++ app/(flights)/flight/[flightId]/page.tsx
@@ -35,6 +35,7 @@ function SingleOfferPage({ params }: { params: { flightId: number } }) {
           <div className="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-gray-900"></div>
         )}
       </div>
+      {/*  */}
     </>
   );
 }
